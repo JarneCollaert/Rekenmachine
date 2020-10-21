d5df61658f457ef31f3a9007f92dfdc8c71d902e
jarne.pierre.collaert@gmail.com <jarne.pierre.collaert@gmail.com>
2020-10-21 09:25:09 +0200

    Add project files.

---
+++ Rekenmachine/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Rekenmachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int getal1;
+            int getal2;
+            Console.WriteLine("Wat is het eerste getal?");
+            getal1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Wat is het tweede getal?");
+            getal2 = Convert.ToInt32(Console.ReadLine());
+            int som = getal1 + getal2;
+            Console.WriteLine("Dit is de som van de 2 getallen " + som);
+            Console.ReadKey();
+            int verschil = getal1 - getal2;
+            Console.WriteLine("Dit is het verschil van de 2 getallen " + verschil);
+            Console.ReadKey();
+            int vermenigvuldiging = getal1 * getal2;
+            Console.WriteLine("Dit is de vermenigvuldiging van de 2 getallen " + vermenigvuldiging);
+            Console.ReadKey();
+            int deling = getal1 / getal2;
+            Console.WriteLine("Dit is de deling van de 2 getallen " + deling);
+            Console.ReadKey();
+           
+        }
+        
+
+    }
+}
